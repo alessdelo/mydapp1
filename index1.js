9f521c0d7f3d61d9336373675e03847547f2c718
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-14 19:53:28 +0100

    Update index1.js
    tried to exclude path module after repeated "internal server error"

--- index1.js
+++ index1.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+// var path = require('path');
 
 const PORT = process.env.PORT || 5000
 
 
 app.get('/', function(req, res) {
     // res.sendFile(path.join('' + 'index1.html'));
-    res.sendFile(path.join('','index.html'));
+    // res.sendFile(path.join('','index.html'));
+    res.sendFile('index.html');
 });
 
 app.listen(PORT);
