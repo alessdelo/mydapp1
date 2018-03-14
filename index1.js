cc9988f2264c69a4cee1b24d53ea19feeacb5148
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-14 20:04:23 +0100

    Update index1.js
    changed script taking inspiration from heroku Getting Started tutorial (file  index.js)

--- index1.js
+++ index1.js
@@ -1,14 +1,21 @@
-var express = require('express');
-var app = express();
-// var path = require('path');
-
+const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 
+express()
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+/*
+
+var app = express();
 app.get('/', function(req, res) {
     // res.sendFile(path.join('' + 'index1.html'));
     // res.sendFile(path.join('','index.html'));
-    res.sendFile('index.html');
+    ///res.sendFile('index.html');
+
 });
 
 app.listen(PORT);
+*/
+
