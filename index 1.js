a583932b46257395cf299649c78a2ee79a0ea91a
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-14 06:57:31 +0100

    Update index 1.js
    changed local port with Heroku port (5000)

--- index 1.js
+++ index 1.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+const PORT = process.env.PORT || 5000
+
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080);
+app.listen(PORT);
