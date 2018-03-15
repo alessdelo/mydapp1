7c6a9cd3b77ab808ff27e2dcf4b6306fb101f12b
alessdelo <33528892+alessdelo@users.noreply.github.com>
2018-03-15 17:59:24 +0100

    Update index1.js
    removed some unuseful comments

--- index1.js
+++ index1.js
@@ -8,16 +8,4 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-/*
-
-var app = express();
-app.get('/', function(req, res) {
-    // res.sendFile(path.join('' + 'index1.html'));
-    // res.sendFile(path.join('','index.html'));
-    ///res.sendFile('index.html');
-
-});
-
-app.listen(PORT);
-*/
 
