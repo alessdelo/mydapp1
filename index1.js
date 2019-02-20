dd75e1a8354050dea3aac10a7f4bf047cd4a36fb
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-02-20 18:38:15 +0100

    Update index1.js

--- index1.js
+++ index1.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 
 
 express()
+  .use('/static', express.static(path.join(__dirname, 'public')))
   .set('views', './views')
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
