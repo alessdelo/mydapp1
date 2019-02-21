cb729b4556bcb69e49eadb73b09312b5629cb411
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-02-21 20:24:29 +0100

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const Web3 = require('web3')
-// connect to Infura node
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/' + INFURA_ROPSTEN_KEY))
 
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-
+/*
+const Web3 = require('web3')
+// connect to Infura node
+const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/' + INFURA_ROPSTEN_KEY))
+*/
 express()
   .use('/static', express.static(path.join(__dirname, 'public')))
   .set('views', './views')
