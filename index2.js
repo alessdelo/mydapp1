e3d200f21cbb4bbb6828ab862ba00240c01aa19c
alessdelo <33528892+alessdelo@users.noreply.github.com>
2019-03-31 12:10:49 +0200

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const smartContractAddress = process.env.WALLET_ADDRESS
+
+const Web3 = require('web3')
+// retrieves infura key from Heroku constants
+// Ropsten
+// const infuraRopstenKey = process.env.INFURA_ROPSTEN_KEY
+// Rinkeby
+const infuraRinkebyKey = process.env.INFURA_RINKEBY_KEY
+
+// connect to Infura node
+// Ropsten
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/' + infuraRopstenKey))
+// Rinkeby
+// const InfuraWeb3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + infuraRinkebyKey))
+
+
+express()
+  .use('/static', express.static(path.join(__dirname, 'public')))
+  .set('views', './views')
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
